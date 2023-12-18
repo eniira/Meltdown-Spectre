c272738b455db1a9bb9ac2b65c3756e26706bdbc
eniira <catarinecruz@alunos.utfpr.edu.br>
2023-12-17 23:47:57 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MeltdownComponent } from './pages/meltdown/meltdown.component';
+import { SpectreComponent } from './pages/spectre/spectre.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'meltdown', component: MeltdownComponent },
+  { path: 'spectre', component: SpectreComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MeltdownComponent } from './pages/meltdown/meltdown.component';
+import { InstructionComponent } from './pages/instruction/instruction.component';
+import { PipelineComponent } from './pages/pipeline/pipeline.component';
+import { ProbeArrayComponent } from './pages/probe-array/probe-array.component';
+import { CacheComponent } from './pages/cache/cache.component';
+import { SpectreComponent } from './pages/spectre/spectre.component';
+import { CodigoComponent } from './pages/codigo/codigo.component';
+import { Cache1Component } from './pages/cache1/cache1.component';
+import { Array2Component } from './pages/array2/array2.component';
+import { HomeComponent } from './pages/home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeltdownComponent,
+    InstructionComponent,
+    PipelineComponent,
+    ProbeArrayComponent,
+    CacheComponent,
+    SpectreComponent,
+    CodigoComponent,
+    Cache1Component,
+    Array2Component,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
